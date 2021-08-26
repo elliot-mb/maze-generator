1e968631b6895f57979858c171e82a31dc2725ea
unknown <elliotbuckingham14@gmail.com>
2021-08-26 17:28:33 +0100

    massively optimised rendering for huge mazes

--- src/main.js
+++ src/main.js
@@ -6,51 +6,10 @@ var ctx = canvas.getContext("2d"); //sets renderer context
 
 let maze = new Maze(canvas.width, canvas.height, [1,1]);
 let dt, pt;
-let frame, data, index, frameID = 0;
+let frame, data, index, frameID = 0, speed = 1;
 
 initialize();
 maze.initialize();
-
-function pushFrame(){
-
-    frame = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    data = frame.data;
-
-    for(let j = 0; j < canvas.height; j++){
-        for(let i = 0; i < canvas.width; i++)
-        {
-            if(maze.cells[j][i]){
-                data = colour([i, j], maze.palette.path, data);
-            }else{ data = colour([i, j], maze.palette.wall, data); }
-        }
-    }
-
-    maze.stack.forEach(cell => {
-        data = colour(cell, maze.palette.stack, data);
-    });
-
-    if(maze.stack.length > 0){ 
-        data = colour(maze.stack[maze.stack.length - 1], maze.palette.current, data);
-    } //set blue
-
-    data = colour(maze.entrance, maze.palette.entrance, data);
-    data = colour(maze.exit, maze.palette.exit, data);
-
-    ctx.putImageData(frame, 0, 0);
-
-}
-
-function colour(coords, rgb, data){
-
-    let index = (coords[0] + coords[1] * canvas.width) * 4;
-
-    data[index] = rgb.r;
-    data[index + 1] = rgb.g;
-    data[index + 2] = rgb.b;
-
-    return data;
-
-}
 
 function initialize(){
 
@@ -74,11 +33,13 @@ function mainLoop(timestamp){
     dt = timestamp - pt;
     pt = timestamp;
 
-    for(let i = 0; i < 10; i++){
-        if(maze.stack.length > 0){ maze.generate(); }
+    for(let i = 0; i < speed; i++){
+        if(maze.stack.length > 0){ 
+            maze.generate();
+        }
     }
 
-    if(frameID % 1 == 0){ pushFrame(); } //displays maze cells to canvas
+    maze.draw(ctx);
 
     frameID++;
     requestAnimationFrame(mainLoop);
--- src/maze.js
+++ src/maze.js
@@ -6,22 +6,29 @@ export default class Maze{
         this.cells = []; //let false be a wall, and true be a path cell (analogous to whether a cell is traversable or not)
         this.stack = []; //stack of cell grid references
         this.size = {w: w, h: h};
+
         this.current; //current cell
-        this.complete = false;
+        this.next; //next cell
+
         this.start = start;
 
         // aesthetic attributes 
         this.palette = 
         { 
-            path: {r: 25, g: 25, b: 25},
+            path: {r: 125, g: 125, b: 125},
             wall: {r: 0, g: 0, b: 0},
             entrance: {r: 255, g: 10, b: 10},
             stack: {r: 100, g: 100, b: 255},
             current: {r: 255, g: 255, b: 0},
             exit: {r: 0, g: 255, b: 10}
         };
+
+        // entrance and exit attributes
         this.entrance = [w - 2, 1];
         this.exit = [1, h - 2];
+
+        this.renderQueue = [];
+        this.backsteps = 0;
 
     }
 
@@ -47,7 +54,8 @@ export default class Maze{
 
     generate(){ //one step of the generation (randomized depth-first search)
 
-        let valids, choice, next;
+        let valids, choice;
+        this.backsteps++;
 
         // look at last visited cell 
         // if backtracking, pop from stack until either you find a direction you can go in, or return to the start
@@ -56,6 +64,7 @@ export default class Maze{
         this.current = this.stack.pop(); 
 
         valids = this.validNeighbours(this.current);
+
         //removes nulls
         for(let i = 0; i < valids.length; i++){
             if(valids[i] == null){ 
@@ -66,7 +75,8 @@ export default class Maze{
 
         //console.log(valids);
 
-        if(valids.length > 0){ //if its found somewhere to go
+        if(valids.length > 0){ //if it's found somewhere to go
+            this.backsteps = 0;
 
             this.stack.push(this.current); //add the current cell to stack
 
@@ -74,19 +84,36 @@ export default class Maze{
             //console.log(choice, valids);
             try{ this.cells[valids[choice][1]][valids[choice][0]] = true; } catch(e) { console.log(this.stack); } //set the choice to a pathway
             
-            next = [valids[choice][0], valids[choice][1]];
+            this.next = [valids[choice][0], valids[choice][1]];
 
-            this.stack.push(next); //add the pathway to the stack (gets popped in the next iteration)
-            this.entranceExit(next);
-
+            this.stack.push(this.next); //add the pathway to the stack (gets popped in the next iteration)
         }
+
+        this.visuals();
+
+    }
+
+    bias(valids, dirWeights){ //adds another of every direction you want to bias
+
+        for(let i = 0; i < valids.length; i++){
+            for(let j = 0; j < dirWeights.length; j++){
+                if(`${valids[i]}` == `${dirWeights[j][0]}`){ 
+                    for(let k = 1; k < dirWeights[j][1]; k++){ // ok yeah three for loops is bad but the most iterations it will ever do is 3 * 4 * n where n is the average weighting of all directions
+                        valids.splice(i + 1, 0, valids[i]);
+                        //console.log("biased");
+                        i++;
+                    }
+                }
+            }
+        }
+
+        return valids;
 
     }
 
     validNeighbours(cell){
 
         let x = cell[0], y = cell[1];
-        let tx, ty; //temp values
 
         // possible directions
         let valids = [[x + 1, y], 
@@ -111,19 +138,79 @@ export default class Maze{
         try{ if(this.cells[y + 1][x - 2] || this.cells[y][x - 2] || this.cells[y - 1][x - 2] || this.cells[y][x - 2] == undefined){ valids[2] = null; } }catch(e){ valids[2] = null; }
         try{ if(this.cells[y - 2][x - 1] || this.cells[y - 2][x] || this.cells[y - 2][x + 1] || this.cells[y - 2][x] == undefined){ valids[3] = null; } }catch(e){ valids[3] = null; }
 
+        valids = this.bias(valids, [ //direction, number of times direction appears in valids list (effective weighting)
+                                   [[x + 1, y], 2], //right
+                                   [[x, y + 1], 1], //down
+                                   [[x - 1, y], 2], //left
+                                   [[x, y - 1], 1]  //up
+                                   ]);
+
         return valids;
     }
 
-    entranceExit(cell){
+    entranceExit(){
 
-        if(cell[0] >= this.exit[0] && cell[1] <= this.exit[1]){
-            this.exit = cell;
+        if(this.next[0] >= this.exit[0] && this.next[1] <= this.exit[1]){
+            this.exit = this.next;
         }
 
-        if(cell[0] <= this.entrance[0] && cell[1] >= this.entrance[1]){
-            this.entrance = cell;
+        if(this.next[0] <= this.entrance[0] && this.next[1] >= this.entrance[1]){
+            this.entrance = this.next;
         }
 
+    }
+
+    visuals(){
+
+        this.entranceExit();
+
+        let render = [{coords: this.current, rgb: this.backsteps > 0 ? this.palette.path : this.palette.stack}, 
+                      {coords: this.entrance, rgb: this.palette.entrance}, 
+                      {coords: this.exit, rgb: this.palette.exit},
+                      {coords: this.next, rgb: this.backsteps > 0 ? this.palette.path : this.palette.current}];
+        this.renderQueue.push(render); //pushes to queue incase we are doing many calculation steps per frame
+
+    }
+
+    draw(ctx){
+
+        let frame = ctx.getImageData(0, 0, this.size.w, this.size.h);
+        let data = frame.data;
+
+        let renders = this.renderQueue.length;
+        let render;
+
+        for(let i = 0; i < renders; i++){
+
+            render = this.renderQueue.shift();
+
+            //optimised to only update 2 pixels for every render instead of the whole screen
+            data = this.colour(render[3].coords, render[3].rgb, 
+                this.colour(render[0].coords, render[0].rgb, data));
+        
+            if(this.stack.length == 0){
+                data = this.colour(render[1].coords, render[1].rgb, 
+                    this.colour(render[2].coords, render[2].rgb, data));
+            }
+        }
+
+        ctx.putImageData(frame, 0, 0);
+    }
+
+    colour(coords, rgb, data){
+
+        let index = this.toIndex(coords);
+    
+        data[index] = rgb.r;
+        data[index + 1] = rgb.g;
+        data[index + 2] = rgb.b;
+    
+        return data;
+    
+    }
+
+    toIndex(coords){ // converts a grid reference to an index in the flat pixel array of the canvas
+        return (coords[0] + coords[1] * this.size.w) * 4; 
     }
 
 }
